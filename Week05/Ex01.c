ba8f5c21853f735f2d4a7f832b5e66ed4d2b5ca0
Nilum <sachithyanilum@gmail.com>
2025-09-18 21:10:59 +0530

    last week updated

--- Week05/Ex01.c
+++ Week05/Ex01.c
@@ -6,7 +6,7 @@
      return n * factorial(n- 1);
  }
  int main() {
-     int n = 10; // Try changing this value and compile with different optimization flags
+    int n = 10; // Try changing this value and compile with different optimization flags
      int result = factorial(n);
      printf("%d\n", result);
      return 0;
