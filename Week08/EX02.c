b92baf0e08990366283ffbbcf1275a4268bdcdfb
Nilum <sachithyanilum@gmail.com>
2025-09-26 08:22:41 +0530

    Code updated

---
+++ Week08/EX02.c
@@ -0,0 +1,23 @@
+#include "ripes_system.h"
+#include <stdio.h>
+#define W LED_MATRIX_0_WIDTH
+#define H LED_MATRIX_0_HEIGHT
+unsigned* led_current = LED_MATRIX_0_BASE;
+unsigned* d_pad_right = D_PAD_0_RIGHT;
+unsigned* d_pad_down = D_PAD_0_DOWN;
+unsigned led_start = LED_MATRIX_0_BASE;
+void main() {
+    *(led_current) = 0xFF << 16;
+    while (1) {
+        if (*d_pad_right & 0x1==1){
+            *(led_current)=0;
+            *(led_current+1) = 0xFF << 16;
+            led_current++;
+        }else if(*d_pad_down & 0x1){
+            *(led_current)=0;
+            *(led_current+W) = 0xFF << 16;
+            led_current = led_current+W;
+        }
+    // Implement the code for other 2 buttons of the D-Pad
+    }
+}
